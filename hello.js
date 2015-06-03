7838b771c9f99402aa2991d5d3223833a970e870
Brett Minnie <github@bdb-studios.co.uk>
2015-06-03 13:41:52 +0100

    Added routing examples

--- hello.js
+++ hello.js
@@ -6,10 +6,22 @@ app.get('/', function(request, result) {
     result.send('Hello world');
 });
 
+app.post('/', function(request, result) {
+    result.send('Received post verb');
+});
+
+app.put('/', function(request, result) {
+    result.send('Received put verb');
+});
+
+app.delete('/', function(request, result) {
+    result.send('Received delete verb');
+});
+
 var server = app.listen(3000, function(){
 
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Server listening on ' + host + ':' + port);
+    console.log('Server listening on http://%s:%s', host, port);
 });
